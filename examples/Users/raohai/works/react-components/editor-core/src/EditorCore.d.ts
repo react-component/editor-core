9aa673c20050218ce5c80b0d1b80f25e485834c9
RaoHai <surgesoft@gmail.com>
2016-05-31 16:55:26 +0800

    Updates

---
+++ examples/Users/raohai/works/react-components/editor-core/src/EditorCore.d.ts
@@ -0,0 +1,23 @@
+/// <reference path="../typings/tsd.d.ts" />
+/// <reference path="interfaces.d.ts" />
+import * as React from 'react';
+import { EditorState } from 'draft-js';
+import { EditorProps, EditorCoreState, Plugin } from "./interfaces";
+declare class EditorCore extends React.Component<EditorProps, EditorCoreState> {
+    state: EditorCoreState;
+    constructor(props: EditorProps);
+    refs: {
+        [string: string]: any;
+        editor?: any;
+    };
+    focus(): void;
+    getPlugins(): Array<Plugin>;
+    getEventHandler(): Object;
+    onChange(editorState: any): void;
+    getEditorState(): EditorState;
+    setEditorState(editorState: any): void;
+    handleKeyBinding(command: String): boolean;
+    generatorEventHandler(eventName: any): Function;
+    render(): JSX.Element;
+}
+export default EditorCore;
