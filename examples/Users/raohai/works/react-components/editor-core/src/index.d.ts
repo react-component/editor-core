0738fe3cf2e869119094b19e17776c5bd72342c2
RaoHai <surgesoft@gmail.com>
2016-06-29 19:40:28 +0800

    Updates

--- examples/Users/raohai/works/react-components/editor-core/src/index.d.ts
+++
@@ -1,6 +0,0 @@
-import EditorCore from './EditorCore';
-declare const EditorCorePublic: {
-    EditorCore: typeof EditorCore;
-    GetText: (editorState: any) => String;
-};
-export default EditorCorePublic;
