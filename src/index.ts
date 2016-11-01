1d70faa0055f990781444d19c4b033ab4448f856
RaoHai <surgesoft@gmail.com>
2016-11-01 11:44:35 +0800

    remove console.error

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,6 @@ import { EditorState } from 'draft-js';
 // import createPlugin from './EditorCore/createPlugin';
 // export this package's api
 
-/*eslint-disable*/
-console.error = (function() {
-  var error = console.error;
-  return function(exception) {
-    if ((exception + '').indexOf('Warning: A component is `contentEditable`') != 0) {
-      error.apply(console, arguments)
-    }
-  }
-})();
-/*eslint-enable*/
-
 import EditorCore from './EditorCore';
 
 const EditorCorePublic = {
