3d23f3fdb4c421110d5aa2d3e7ee5d80acf31efb
RaoHai <surgesoft@gmail.com>
2017-05-18 11:37:41 +0800

    fix proptype issue

--- src/Toolbar/Toolbar.tsx
+++ src/Toolbar/Toolbar.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { Map, List } from 'immutable';
 import { Plugin } from '../EditorCore';
 import { EditorState } from 'draft-js';
--- src/Toolbar/ToolbarLine.tsx
+++ src/Toolbar/ToolbarLine.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 export default class ToolbarLine extends React.Component<any, any> {
   render() {
     return <div>{this.props.children}</div>
